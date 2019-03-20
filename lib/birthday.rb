aaa4f58e496a8789501e7a582dd48da32d4a76fe
Rob O'Gara <rgo594@gmail.com>
2019-03-20 19:44:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,6 +14,7 @@ def age_appropriate_birthday(birthday_kids)
   if age > 12
     birthday_kids.each do |kids_name, age|
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
   end
 end
 
